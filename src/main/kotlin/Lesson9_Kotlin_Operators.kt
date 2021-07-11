0f27c8a11e1fe5e50b11c54235607b9012fe9274
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-11 11:31:12 -0300

    Lesson 9 add

---
+++ src/main/kotlin/Lesson9_Kotlin_Operators.kt
@@ -0,0 +1,36 @@
+fun operationsExplication() {
+    /*
+          Kotlin divides the operators into the following groups:
+
+            ° Arithmetic operators --> This is very easy; its generic for all PLs
+            ° Assignment operators
+            ° Comparison operators --> This is very easy; its generic for all PLs
+            ° Logical operators
+
+        # Kotlin Assignment Operators #
+
+            A list of all assignment operators:
+
+            Operator	Example	    Same As
+                =	    x = 10	    x = 10
+                +=	    x += 30	    x = x + 30
+                -=	    x -= 30	    x = x - 30
+                *=	    x *= 30	    x = x * 30
+                /=	    x /= 30	    x = x / 30
+                %=	    x %= 30	    x = x % 30
+
+        # Kotlin Logical Operators #
+
+            Logical operators are used to determine the logic between variables or values:
+
+            Operator	Name	   Description	                                            Example
+              && 	Logical and	   Returns true if both statements are true	            x < 5 &&  x < 10
+              || 	Logical or	   Returns true if one of the statements is true	    x < 5 || x < 4
+              !	    Logical not	   Reverse the result                                       x ! y
+
+     */
+}
+
+fun main(){
+    operationsExplication() // I know, this don't nothing ! LOL
+}
