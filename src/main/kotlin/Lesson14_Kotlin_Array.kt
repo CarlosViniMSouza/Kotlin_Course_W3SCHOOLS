e86fca1bc50b91b2bb2ca08efb61178f8dedf651
CarlosViniMSouza <vinicius.souza5530@gmail.com>
2021-07-13 11:27:25 -0300

    Lessons Arrays ok

--- src/main/kotlin/Lesson14_Kotlin_Array.kt
+++ src/main/kotlin/Lesson14_Kotlin_Array.kt
@@ -5,8 +5,31 @@ fun proofArrays() {
         variables for each value.
 
         To create an array, use the arrayOf() function, and place the values in a comma-separated
-        list inside it:
+        list inside it.
      */
+
+    var listAny = arrayOf("Medicine", "Law", "Engineering", "Social Science", "Administration", "Marketing")
+
+    println("My course preference is: " + listAny[2])
+
+    // To find out how many elements an array have, use the size property:
+
+    print("The length this array is: " + listAny.size + "\n")
+
+    // You can use the in operator to check if an element exists in an array:
+
+    if ("Administration" in listAny) {
+        println("Ok, We have your course!")
+    } else {
+        println("Sorry, we haven't this course!")
+    }
+
+    // You can loop through the array elements with the for loop, which you will learn
+    // even more about in the next chapter.
+
+    for (courses in listAny) {
+        println(courses)
+    }
 }
 
 fun main() {
